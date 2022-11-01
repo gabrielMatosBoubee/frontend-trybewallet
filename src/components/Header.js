95d4c252ed8f59232c59af90c3b066732e828162
gabrielMatosBoubee <gm.boubee@gmail.com>
2022-11-01 16:39:55 -0300

    terminei o header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  state = {
+    expense: 0,
+    exchange: 'BRL',
+  };
+
   render() {
+    const { expense, exchange } = this.state;
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <div>
+        <span data-testid="email-field">{email}</span>
+        <p data-testid="total-field">{expense}</p>
+        <p data-testid="header-currency-field">{exchange}</p>
+      </div>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (globalStore) => ({
+  email: globalStore.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
