57a5e3a997d02879611ab78c9fdadaf8e146e354
gabrielMatosBoubee <gm.boubee@gmail.com>
2022-11-02 18:03:07 -0300

    terminei o req 06

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
-      <div>Table</div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+          {/* <tbody>
+            { expenses.map((element) => ((
+              <tr key={ element.id }>
+                <td>{element.description}</td>
+                <td>{element.tag}</td>
+                <td>{element.payment}</td>
+                <td>{element.value}</td>
+                <td>{element.currency}</td>
+                <td>{element.method}</td>
+                <td>{element.exchangeRates[element.currency]}</td>
+                <td>
+                  {
+                    Number(element.exchangeRates[element.currency])
+                    * Number(element.value)
+                  }
+                </td>
+                <td>Real</td>
+                <button type="button">Editar/Excluir</button>
+              </tr>
+            ))) }
+          </tbody> */}
+        </table>
+      </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (globalState) => ({
+  expenses: globalState.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
